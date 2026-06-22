883904e92c93b63b4ff2812215f36c41ad134738
Aishi De <aishibde@gmail.com>
2026-06-23 04:44:29 +0530

    Basic structure of PL/SQL

---
+++ PLSQL_intro_and_basics.sql
@@ -0,0 +1,18 @@
+--AISHI DE 
+
+/*
+PL/SQL INTRO AND BASICS 
+*/
+
+
+-- Structure of PL/SQL Block  : 
+
+DECLARE
+    --declare statement 
+BEGIN
+    --executable statement 
+EXCEPTION
+    --exception handling statements 
+END;
+/
+
