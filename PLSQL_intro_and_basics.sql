08fbfece8403b0cc96adaff4abc0cafa80d3180c
Aishi De <aishibde@gmail.com>
2026-06-23 05:09:51 +0530

    take input

--- PLSQL_intro_and_basics.sql
+++ PLSQL_intro_and_basics.sql
@@ -49,4 +49,28 @@ I love GeeksForGeeks
 PL/SQL procedure successfully completed.
 
 
+-- Taking input from users  
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+    --taking input of a 
+    a NUMBER := &a;
+    --taking input of b
+    b VARCHAR2(30) := &b;
+BEGIN
+    NULL;
+END;
+/
+
+OUTPUT :
+
+Enter value for a: 22
+old   2: a number := &a;
+new   2: a number := 22;
+Enter value for b: 'AISHI'
+old   3: b varchar2(30) := &b;
+new   3: b varchar2(30) := 'AISHI';
+
+
 --
