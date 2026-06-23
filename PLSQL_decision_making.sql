88a78c1fccdb754dcdb5d7e18c28871398c207da
Aishi De <aishibde@gmail.com>
2026-06-24 03:40:19 +0530

    add syntax of nested if then

--- PLSQL_decision_making.sql
+++ PLSQL_decision_making.sql
@@ -38,3 +38,15 @@ if (condition) then
 else 
     -- Executes this block if
     -- condition is false
+
+
+
+--NESTED if then 
+
+if (condition1) then
+   -- Executes when condition1 is true
+   if (condition2) then 
+     -- Executes when condition2 is true
+   end if; 
+end if;
+
