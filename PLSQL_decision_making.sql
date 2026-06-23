b979079bc43d39ad10ee5a78e6fbd822b5ffeecd
Aishi De <aishibde@gmail.com>
2026-06-24 03:04:44 +0530

    added syntax and example of if condition in PL/SQL

--- PLSQL_decision_making.sql
+++ PLSQL_decision_making.sql
@@ -5,3 +5,27 @@
 -> NESTED-IF=THEN
 -> IF THEN ELSIF-THEN-ELSE LADDER
 
+
+/*
+IF THEN  : (Syntax) 
+
+if condition then 
+    -- DO SOMETHING 
+end if;
+
+*/
+
+-- Example
+
+declare 
+    -- declare the values here
+begin
+
+    if condition then 
+    dbms_output.put_line('output');
+    end if;
+
+    dbms_output.put_line('output2');
+
+end;
+
