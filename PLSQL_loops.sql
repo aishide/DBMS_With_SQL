64c608a37b116eb10cd045cfd7dccf278491e972
Aishi De <aishibde@gmail.com>
2026-06-24 05:52:46 +0530

    Added the syntax of loops

---
+++ PLSQL_loops.sql
@@ -0,0 +1,19 @@
+-- AISHI DE 
+
+
+--SYNTAX : 
+
+LOOP
+   -- Code block to be executed repeatedly
+END LOOP;
+
+
+-- Exit statment 
+
+LOOP
+   -- Code block
+   IF condition THEN
+      EXIT;
+   END IF;
+END LOOP;
+
