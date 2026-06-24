75882c32c02b1eab55f3012fc7f297fbe6d7ca2e
Aishi De <aishibde@gmail.com>
2026-06-25 04:50:52 +0530

    Added synatx of fpr loop

---
+++ PLSQL_for_loop.sql
@@ -0,0 +1,15 @@
+-- AISHI DE 
+
+syntax 
+
+DECLARE
+        --declare loop variable and provide its datatype
+loop_varaible datatype;
+BEGIN
+        --for loop with start and end value
+FOR loop_variable in start_value .. end_value LOOP
+set of statements 
+END LOOP;
+END;
+/
+
