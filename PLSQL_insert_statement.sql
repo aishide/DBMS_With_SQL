15ccebaa5c885b4fa1a5085585c9dc719e0a3dfd
Aishi De <aishibde@gmail.com>
2026-06-26 04:52:06 +0530

    Added example of insert into statement

--- PLSQL_insert_statement.sql
+++ PLSQL_insert_statement.sql
@@ -20,3 +20,21 @@ VALUES (101, 'John', 'Doe', 'Sales');
 
 
 
+Example : 
+
+INSERT INTO employees (employee_id, first_name, last_name, department)
+VALUES
+(102, 'Alice', 'Smith', 'Marketing'),
+(103, 'Bob', 'Johnson', 'Finance'),
+(104, 'Emily', 'Brown', 'HR');
+
+CREATE TABLE New_employees (
+employee_id INT,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+department VARCHAR(50)
+);
+
+INSERT INTO New_employees
+SELECT * FROM employees
+WHERE 1 != 2;
