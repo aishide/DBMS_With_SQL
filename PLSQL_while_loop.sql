f17c6d87c3dade35531d47c90baf3898414e966c
Aishi De <aishibde@gmail.com>
2026-06-25 07:12:29 +0530

    Added example of while loop

--- PLSQL_while_loop.sql
+++ PLSQL_while_loop.sql
@@ -9,3 +9,25 @@ LOOP
    -- Statements to be executed as long as the condition is true
 END LOOP;
 
+
+
+
+Example :
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+    counter NUMBER := 1;
+
+BEGIN 
+
+    WHILE counter <= 5
+    LOOP
+        dbms_output.put_line('counter in while loop : ' || counter);
+        counter := counter+1;
+
+    END LOOP;
+
+END;
+/
+
