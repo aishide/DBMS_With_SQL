b0cb3927f991e195da58cf93913f476099b2f3d1
Aishi De <aishibde@gmail.com>
2026-06-26 04:40:15 +0530

    Added the query

--- PLSQL_insert_statement.sql
+++ PLSQL_insert_statement.sql
@@ -6,4 +6,17 @@ INSERT INTO table_name (column1, column2, ...)
 VALUES (value1, value2, ...);
 
 
-Query 
+Query :
+
+CREATE TABLE employees (
+    employee_id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department VARCHAR(50)
+);
+
+INSERT INTO employees (employee_id, first_name, last_name, department)
+VALUES (101, 'John', 'Doe', 'Sales');
+
+
+
