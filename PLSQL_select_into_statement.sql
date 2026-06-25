1a5251eb5a42d5c21db8c1e06c75b1d74aec845a
Aishi De <aishibde@gmail.com>
2026-06-25 08:23:27 +0530

    Added syntax of select into

---
+++ PLSQL_select_into_statement.sql
@@ -0,0 +1,36 @@
+--AISHI DE 
+
+
+
+Syntax : 
+
+SELECT column1,  column2, . . . . , column_n 
+INTO
+variable1, variable2, . . . . , variable_n  
+FROM table 
+WHERE expresion1, expression2, . . . . , expression_n;
+
+
+
+/*
+
+CREATE TABLE EMPLOYEE (
+    EMP_ID INT PRIMARY KEY,
+    NAME VARCHAR(50),
+    AGE INT,
+    SALARY INT
+);
+INSERT INTO EMPLOYEE (EMP_ID, NAME, AGE, SALARY) VALUES
+(001, 'Sahil', 21, 15000),
+(002, 'Alen', 22, 13000),
+(003, 'John', 22, 14000),
+(004, 'Alex', 20, 13000),
+(005, 'Mathew', 22, 14000),
+(006, 'Sia', 21, 15000),
+(007, 'David', 22, 16000),
+(008, 'Tim', 21, 14000),
+(009, 'Leo', 20, 15000),
+(010, 'Tom', 21, 16000);
+
+*/
+
