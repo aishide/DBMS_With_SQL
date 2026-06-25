dd0e839de0a96358230c31dc4e65ae0332b1b1d2
Aishi De <aishibde@gmail.com>
2026-06-26 05:29:39 +0530

    Added example 1

--- PLSQL_update_statement.sql
+++ PLSQL_update_statement.sql
@@ -9,3 +9,31 @@ SET column1 = value1,
 WHERE condition;
 
 
+
+Example  :
+
+CREATE TABLE salary_adjustments (
+    employee_id NUMBER PRIMARY KEY,
+    new_salary NUMBER(10, 2)
+);
+
+-- Insert some sample salary adjustments
+INSERT INTO salary_adjustments (employee_id, new_salary) VALUES (1001, 58000);
+INSERT INTO salary_adjustments (employee_id, new_salary) VALUES (1003, 82000);
+INSERT INTO salary_adjustments (employee_id, new_salary) VALUES (1005, 64000);
+
+
+
+
+BEGIN
+  UPDATE employees
+  SET salary = salary * 1.1
+  WHERE employee_id = 1002;
+
+  COMMIT; -- Commit the changes to make them permanent
+
+  DBMS_OUTPUT.PUT_LINE('Salary updated for employee 1002.');
+END;
+/
+
+
