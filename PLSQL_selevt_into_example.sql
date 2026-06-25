a899867ceb395a16e1078605244d4d0a426dcc42
Aishi De <aishibde@gmail.com>
2026-06-26 03:17:19 +0530

    Added one example of select into statment

---
+++ PLSQL_selevt_into_example.sql
@@ -0,0 +1,21 @@
+SET SERVEROUTPUT ON;
+
+DECLARE
+    new_cid HR.COUNTRIES.country_id%type;
+    new_cname HR.COUNTRIES.country_name%type;
+
+BEGIN 
+    SELECT country_id , country_name 
+    INTO new_cid , new_cname
+    FROM HR.COUNTRIES
+    WHERE COUNTRY_ID = 'IN';
+
+    dbms_output.put_line(new_cid);
+    dbms_output.put_line(new_cname);
+
+EXCEPTION 
+    WHEN NO_DATA_FOUND THEN 
+        dbms_output.put_line('Sorry data is not there');
+
+END;
+/
