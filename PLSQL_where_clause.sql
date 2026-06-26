0a7a94c8b71e9db344ef93346c51893f41aadf49
Aishi De <aishibde@gmail.com>
2026-06-26 06:02:57 +0530

    Added example of where clause in select statement

---
+++ PLSQL_where_clause.sql
@@ -0,0 +1,17 @@
+-- AISHI DE 
+
+
+Syntax :
+
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition;
+
+
+
+-- Where clause in select statement 
+SELECT name, department, salary
+FROM employees
+WHERE department = 'IT';
+
+--
