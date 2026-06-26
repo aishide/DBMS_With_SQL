593322406ef2d9df4f1f175af710e9542e22ff27
Aishi De <aishibde@gmail.com>
2026-06-26 06:27:32 +0530

    Added syntax

---
+++ PLSQL_group_by.sql
@@ -0,0 +1,10 @@
+-- AISHI DE 
+
+Syntax : 
+
+SELECT expression1, expression2, ... expression_n,   
+aggregate_function (aggregate_expression)  
+FROM tables  
+WHERE conditions  
+GROUP BY expression1, expression2, ... expression_n;
+
