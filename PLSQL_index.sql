79dd291ddb92175b22b3697f01dc5f46ed0606a9
Aishi De <aishibde@gmail.com>
2026-06-27 01:09:51 +0530

    Added syntax :

--- PLSQL_index.sql
+++ PLSQL_index.sql
@@ -1,2 +1,13 @@
 -- AISHI DE 
 
+TYPES OF PL/SQL INDEX :
+Single Column Index: Created on a single column of a table
+Composite Index: An index on multiple columns
+Unique Index: Ensures that the indexed column has unique values.
+Clustered Index: Arranges the data rows in the table according to the index order.
+
+
+SYNTAX :
+CREATE INDEX Index_Name ON Table_Name ( Column_Name);  
+
+
