7e9f02387a45aa9964a9c8701cb598acfa1f5963
Aishi De <aishibde@gmail.com>
2026-06-28 02:15:29 +0530

    Added exception handling basic theory

---
+++ PLSQL_exception_handling.sql
@@ -0,0 +1,16 @@
+-- AISHI DE 
+
+An exception is an error which disrupts the normal flow of program instructions.
+
+
+There are two types of exceptions defined in PL/SQL
+
+User defined exception.
+System defined exceptions.
+
+
+-- 1. System-Defined Exceptions
+
+These are predefined exceptions that occur when Oracle rules or constraints 
+are violated. They include NO_DATA_FOUND, ZERO_DIVIDE, TOO_MANY_ROWS, etc.
+
