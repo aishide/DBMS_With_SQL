f350fbccdadfd85f8d599d64e3ea835a4998f572
Aishi De <aishibde@gmail.com>
2026-06-28 02:05:14 +0530

    Added before insert and before delete trigger

--- PLSQL_triggers.sql
+++ PLSQL_triggers.sql
@@ -44,3 +44,61 @@ END;
 Query operation to be performed i.e INSERT,DELETE,UPDATE.
 
 
+
+
+
+
+CONDITIONAL TRIGGER  :
+
+-- Create Geeks table
+CREATE TABLE Geeks (
+    Id INT,
+    Name VARCHAR2(20),
+    Score INT
+);
+
+-- Insert into Geeks Table 
+INSERT INTO Geeks (Id, Name, Score) VALUES (1, 'Sam', 800);
+INSERT INTO Geeks (Id, Name, Score) VALUES (2, 'Ram', 699);
+INSERT INTO Geeks (Id, Name, Score) VALUES (3, 'Tom', 250);
+INSERT INTO Geeks (Id, Name, Score) VALUES (4, 'Om', 350);
+INSERT INTO Geeks (Id, Name, Score) VALUES (5, 'Jay', 750);
+-- insert statement should be written for each entry in Oracle Sql Developer
+
+CREATE TABLE Affect (
+    Id INT,
+    Name VARCHAR2(20),
+    Score INT
+);
+
+
+Before :
+
+-- BEFORE INSERT trigger
+CREATE OR REPLACE TRIGGER BEFORE_INSERT
+BEFORE INSERT ON Geeks
+FOR EACH ROW
+BEGIN
+    INSERT INTO Affect (Id, Name, Score)
+    VALUES (:NEW.Id, :NEW.Name, :NEW.Score);
+END;
+/
+INSERT INTO Geeks (Id, Name, Score) VALUES (6, 'Arjun', 500);
+
+
+
+BEFORE DELETE Trigger :
+
+-- BEFORE DELETE trigger
+CREATE OR REPLACE TRIGGER BEFORE_DELETE
+BEFORE DELETE ON Geeks
+FOR EACH ROW
+BEGIN
+    INSERT INTO Affect (Id, Name, Score)
+    VALUES (:OLD.Id, :OLD.Name, :OLD.Score);
+END;
+/
+DELETE FROM Geeks WHERE Id = 3;
+
+
+
