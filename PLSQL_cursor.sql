2334f536a4a6f6e2c0c8b15aeb32b6e5e5f349ef
Aishi De <aishibde@gmail.com>
2026-06-28 03:05:35 +0530

    Added cursor theory

---
+++ PLSQL_cursor.sql
@@ -0,0 +1,8 @@
+-- AISHI DE 
+
+A Cursor in PL/SQL is a pointer to a context area that
+ stores the result set of a query.
+
+The cursor is used to retrieve data one row at a time from the results set, 
+unlike other SQL commands that operate on all rows at once.
+
