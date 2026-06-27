22c908af8d242e69f2a6058f75dadd2a6a366838
Aishi De <aishibde@gmail.com>
2026-06-28 02:16:24 +0530

    Added example of system defined exception handling

--- PLSQL_exception_handling.sql
+++ PLSQL_exception_handling.sql
@@ -14,3 +14,21 @@ System defined exceptions.
 These are predefined exceptions that occur when Oracle rules or constraints 
 are violated. They include NO_DATA_FOUND, ZERO_DIVIDE, TOO_MANY_ROWS, etc.
 
+
+Example  : 
+
+DECLARE
+a NUMBER := 10; 
+b NUMBER := 0;  
+c NUMBER;
+BEGIN 
+c := a / b; 
+-- Division by zero  DBMS_OUTPUT.PUT_LINE('Result: ' || c);
+EXCEPTION  
+WHEN ZERO_DIVIDE THEN    
+DBMS_OUTPUT.PUT_LINE('Error: Division by zero is not allowed.');
+END;
+
+Output : Error: Division by zero is not allowed.
+
+
