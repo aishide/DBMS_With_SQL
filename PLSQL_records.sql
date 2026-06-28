054b20ae53902b2aee80a76470eb9af7e720632c
Aishi De <aishibde@gmail.com>
2026-06-28 10:06:29 +0530

    Added example of table based records

--- PLSQL_records.sql
+++ PLSQL_records.sql
@@ -18,3 +18,32 @@ Programmer-Defined Records: User-defined records with custom fields and data typ
 Nested Records: Records within records, enabling hierarchical data management
 
 
+In databases, records are called rows in a table, and we can interact with them using SQL queries.
+
+
+
+-- Example: Declaring and Using a Table-Based Record
+
+record Human {
+  id: Integer,
+  name: String,
+  age: Integer,
+  ph_no: Integer
+}
+
+Now, we are creating instances for this record and inserting the data.
+
+Human1 = Human {
+     id: 1,
+     name: "John",
+     age: 25,
+     ph_no: 9124567833
+}
+Human2 = Human {
+    id: 2,
+    name: "Amar",
+    age: 26,
+    ph_no: 9874567321
+}
+
+
