dc963bc5bc57533cae2526103854e26ecd2fde23
Aishi De <aishibde@gmail.com>
2026-06-28 10:04:47 +0530

    Added theory of records

---
+++ PLSQL_records.sql
@@ -0,0 +1,20 @@
+-- AISHI DE 
+
+In PL/SQL, records are structured data types that allow grouping multiple,
+related data fields under a single name.
+
+Records are similar to structures in C, objects in Java, and tuples in Python. 
+Each record has multiple fields, and each field has a data type and a name. 
+Records help developers organize data logically, making it easier to work with and read. 
+Records can be declared at the package, block, or schema level and used in various PL/SQL contexts.
+
+
+Types of PL/SQL Records
+PL/SQL offers different ways to declare and use records. Here’s an overview of the common types:
+
+Table-Based Records: Linked to a table’s row structure, allowing you to work directly with database table columns.
+Cursor-Based Records: Derived from a cursor’s query result set, providing a way to process query results one row at a time.
+Programmer-Defined Records: User-defined records with custom fields and data types.
+Nested Records: Records within records, enabling hierarchical data management
+
+
